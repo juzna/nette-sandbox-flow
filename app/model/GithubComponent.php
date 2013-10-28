a37903e38eb5afd669973444186292c20f0ecf41
Jan Dolecek <juzna.cz@gmail.com>
2013-10-28 10:06:50 +0100

    code cleanup

--- app/model/GithubComponent.php
+++ app/model/GithubComponent.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nette\Utils\Json;
+
 
 /**
  * Show last updated project
@@ -24,20 +26,21 @@ class GithubComponent extends BaseControl
 	public function renderFlow()
 	{
 		$data = (yield $this->httpClient->request('GET', "https://github.com/$this->name.json")->getResponseBody());
-		$events = Nette\Utils\Json::decode($data, Nette\Utils\Json::FORCE_ARRAY);
+		$events = Json::decode($data, Json::FORCE_ARRAY);
 		if (!isset($events[0])) {
 			yield result("No info");
 		}
 		$event = $events[0];
 
-		$url = str_replace('https://', 'https://raw.', $event['repository']['url']) . '/master/composer.json';
-		$composerData = (yield $this->httpClient->request('GET', $url)->getResponseBody());
+		$composerUrl = str_replace('https://', 'https://raw.', $event['repository']['url']) . '/master/composer.json';
+		$composerData = (yield $this->httpClient->request('GET', $composerUrl)->getResponseBody());
 
 		if ($composer = json_decode($composerData, JSON_OBJECT_AS_ARRAY)) {
 			yield result("Last change to composer project $composer[name]");
 
 		} else {
 			yield result("Last change to github repo {$event['repository']['url']}");
+
 		}
 	}
 
